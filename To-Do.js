244369ecd507b7d68307a2a900b02f241e1377c0
pratiksha950 <salunkepratiksha950@gmail.com>
2025-09-07 19:42:24 +0530

    Scroll bar added

--- To-Do.js
+++ To-Do.js
@@ -2,6 +2,7 @@ const ToDoItems=["a","b","c","d"];
 
 const localCards=()=>{
     const containerElement=document.querySelector("#container");
+    containerElement.innerHTML="";
     for(const ToDoItem of ToDoItems){
         containerElement.innerHTML+=`<div class="item">
         ${ToDoItem} <img src="./Images/trash.png" class="delete-icon">
@@ -10,3 +11,10 @@ const localCards=()=>{
     }
 };
 localCards();
+const AddToDo=()=>{
+    const Taskinput=document.querySelector("#input");
+    const AddToDo =Taskinput.value;
+    ToDoItems.push(AddToDo);
+    Taskinput.value="";
+    localCards();
+}
