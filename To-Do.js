71d64bec333017bf056f545297ce77d90abc4203
pratiksha950 <salunkepratiksha950@gmail.com>
2025-09-07 22:53:56 +0530

    required changes added

--- To-Do.js
+++ To-Do.js
@@ -1,23 +1,32 @@
-const ToDoItems=["a","b","c","d"];
+let ToDoItems=[];
 
-const localCards=()=>{
+const loadCards=()=>{
+     ToDoItems = JSON.parse(localStorage.getItem("todoItems") || "[]");
     const containerElement=document.querySelector("#container");
     containerElement.innerHTML="";
     for(const ToDoItem of ToDoItems){
         containerElement.innerHTML+=`<div class="item">
-        ${ToDoItem} <img src="./Images/trash.png" class="delete-icon">
+        ${ToDoItem} <img src="./Images/trash.png" class="delete-icon" onclick="deleteTodo('${ToDoItem}')">
         </div>`;
-
     }
 };
-localCards();
+loadCards();
 const AddToDo=()=>{
     const Taskinput=document.querySelector("#input");
+    const newTodo= Taskinput.value;
  
     if(newTodo.trim()!=""){
-            todoItems.push(newTodo);
-            taskInput.value="";
-            localStorage.setItem("todoItems",JSON.stringify(todoItems));
-            loadCards();
-           }
+        ToDoItems.push(newTodo);
+        Taskinput.value="";
+        localStorage.setItem("todoItems",JSON.stringify(ToDoItems));
+        loadCards();
         }
+};
+const deleteTodo=(task)=>{
+   const indexOfItem=ToDoItems.indexOf(task);
+   if(indexOfItem>-1){
+    ToDoItems.splice(indexOfItem,1);
+    localStorage.setItem("todoItems",JSON.stringify(ToDoItems));
+    loadCards();
+   }
+}
